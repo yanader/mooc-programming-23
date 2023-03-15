719b1002064179cb10b12a1fa864a69288914c0c
Ste <mrbarrow@hotmail.co.uk>
2023-03-15 19:40:29 +0000

    update

--- part04-12_change_value_of_item/src/change_value_of_item.py
+++ part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,1 +1,10 @@
 # Write your solution here
+ls = [1,2,3,4,5]
+
+while True:
+    index = int(input('Index:'))
+    if index == -1:
+        break
+    value = int(input('New value:'))
+    ls[index] = value
+    print(ls)
