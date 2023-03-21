cc493f7eed0270360d24e973b01de1a117759518
Ste <mrbarrow@hotmail.co.uk>
2023-03-21 23:13:43 +0000

    end of section 5

--- part05-26_student_database/src/student_database.py
+++ part05-26_student_database/src/student_database.py
@@ -1,1 +1,92 @@
 # Write your solution here
+def add_student(database: dict, name: str):
+    database[name] = []
+
+
+def add_course(database: dict, name: str, course: tuple):
+    if course[1] == 0:
+        return
+    course_to_add = course[0]
+    score_to_beat = 0
+    for completed_course in database[name]:
+        if completed_course[0] == course_to_add:
+            score_to_beat = completed_course[1]
+            break
+    if not course_exists(database, name, course):
+        database[name].append(course)
+    elif course[1] > score_to_beat:
+        index_to_remove = index_of_course(database, name, course)
+        del database[name][index_to_remove]
+        database[name].append((course))
+
+
+
+def index_of_course(database: dict, name: str, course: tuple):
+    i = 0
+    while i < len(database[name]):
+        if database[name][i][0] == course[0]:
+            return i
+        i+=1
+    return -1
+
+def summary(database: dict):
+    print(f'students {len(database)}')
+    name, course_count = most_courses(database)
+    print(f'most courses completed {course_count} {name}')
+    name, av = best_average(database)
+    print(f'best average grade {av} {name}')
+
+
+def most_courses(database: dict):
+    course_count = 0
+    name = ''
+    for student in database:
+        if len(database[student]) > course_count:
+            course_count = len(database[student])
+            name = student
+    return name, course_count
+
+def best_average(database: dict):
+    best_av = 0
+    name = ''
+    for student in database:
+        sum = 0
+        for course in database[student]:
+            sum+= course[1]
+        av = sum / len(database[student])
+        if av > best_av:
+            best_av = av
+            name = student
+    return name, best_av
+
+
+
+def course_exists(database: dict, name: str, course: tuple):
+    for completed_course in database[name]:
+        if completed_course[0] == course[0]:
+            return True
+    return False
+
+
+def print_student(database: dict, name: str):
+    if name not in database:
+        print(name + ': no such person in the database')
+    elif len(database[name]) == 0:
+        print(name +':')
+        print(' no completed courses')
+    else:
+        print(name + ':')
+        print(' ' + str(len(database[name])) + ' completed courses:')
+        points = 0
+        for course in database[name]:
+            print('  '+course[0]+' '+str(course[1]))
+            points += course[1]
+        print(' average grade ' + str(points/len(database[name])))
+
+
+
+if __name__ == '__main__':
+    students = {}
+    add_student(students, "Peter")
+    add_course(students, "Peter", ("Introduction to Programming", 5))
+    print_student(students, "Peter")
--- part05-27_letter_square/src/letter_square.py
+++ part05-27_letter_square/src/letter_square.py
@@ -1,1 +1,15 @@
-# Write your solution here
+
+size = int(input('Layers:'))
+letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+square_side_len = size * 2 - 1
+square_mid = (square_side_len // 2) 
+
+i = 0
+while i < square_mid+1:
+    print(letters[size-1:size-1-i:-1] + ((square_side_len - (2*i)) * letters[size-1-i]) + letters[size-i:size:1])
+    i+=1
+j = 0
+while j < size-1:
+    print( letters[size-1:1+j:-1] + ((3+(2*j)) * letters[1+j]) + letters[j+2:size:1] ) 
+    j+=1
+
